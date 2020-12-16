a95518ad4804ad4d9c3c025b73268fc80f5bacd7
Roberto Callejas <rcallejas22415@columbushs.com>
2020-12-16 12:29:47 -0500

    finished

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,8 @@
-// Rover Object Goes Here Iteration 1
+// Rover Object Goes Here 
 let rover = {
-  direction = "N"
+  direction = "N",
+  x: 0,
+  y: 0,
 }
 
 //Iteration 2
@@ -13,10 +15,10 @@ function turnLeft(rover){
   console.log("Direction is S");
 }else if(rover.direction == "S"){
   rover.direction = "E";
-}else if(rover.direction == "E")
+}else if(rover.direction == "E"){
   rover.direction = "N";
+  }
 }
-
 function turnRight(rover){
   if (rover.direction == "N"){
   rover.direction = "E"
@@ -30,34 +32,46 @@ function turnRight(rover){
 }else if(rover.direction == "W"){
   rover.direction = "N"
   console.log("Direction is N");
+  }
 }
 
 function moveForward(rover){
   if (rover.direction == "N"){
-  rover.direction = "S"
-  console.log("Direction is S");
-}else if(rover.direction == "S"){
-  rover.direction = "N"
-  console.log("Direction is N");
-}else if(rover.direction == "W"){
-  rover.direction = "E"
-  console.log("Direction is E");
-}else if(rover.direction == "E"){
-  rover.direction = "W"
-  console.log("Direction is W");
+    rover.y--;
+    console.log(`X ${rover.x}, Y: ${rover.y}`);
+  }else if (rover.direction == "S"){
+    rover.y++;
+    console.log(`X ${rover.x}, Y: ${rover.y}`);
+  }else if (rover.direction == "E"){
+    rover.x++;
+    console.log(`X ${rover.x}, Y: ${rover.y}`);
+  }else if (rover.direction == "W"){
+    rover.x--;
+    console.log(`X ${rover.x}, Y; ${rover.y}`);
+  }
 }
 
-// ======================
+  function commands(steps){
+    // i = 0 -> l => turnLeft()
+    // i = 1 -> l => turnLeft()
+    // i = 2 -> f => moveForward()
+    // i = 3 -> f => moveForward()
+    // i = 4 -> r => moveRight()
+    // i = 5 -> r => moveRight()
+    for(let i = 0; i < steps.length; i++){
+      console.log(steps.charAt(i));
+      turnLeft(rover);
+    }if(steps.charAt(i) == "r"){
+      turnRight(rover);
+    }else if(steps.charAt(i) == "l"){
+      turnLeft(rover);
+    }else if(steps.charAt(i) == "f"){
+      turnForward(rover);
+    }
+    //Push to travel log
+    rover.travelLog.push(steps.charAt(i));
 
-// ======================
-function turnLeft(rover){
-  console.log("turnLeft was called!");
-}
+  }
+  console.log("Travel Log:" + rover.travelLog);
 
-function turnRight(rover){
-  console.log("turnRight was called!");
-}
 
-function moveForward(rover){
-  console.log("moveForward was called")
-}
